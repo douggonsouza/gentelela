12e037a4a43d47147e5f2bdfff0c3e4daffad736
Douglas G. de Souza <douggonsouza@gmail.com>
2023-04-07 16:37:02 -0300

    Novo modelo.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -42,6 +42,19 @@ class ComposerStaticInit174f4cea62571bd2affc3a54a21e306d
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'douggonsouza\\gentelela\\alerts' => __DIR__ . '/../..' . '/src/alerts.php',
+        'douggonsouza\\gentelela\\alertsInterface' => __DIR__ . '/../..' . '/src/alertsInterface.php',
+        'douggonsouza\\gentelela\\assets\\assets' => __DIR__ . '/../..' . '/src/assets/assets.php',
+        'douggonsouza\\gentelela\\behaviorInterface' => __DIR__ . '/../..' . '/src/behaviorInterface.php',
+        'douggonsouza\\gentelela\\benchmarck' => __DIR__ . '/../..' . '/src/benchmarck.php',
+        'douggonsouza\\gentelela\\benchmarckInterface' => __DIR__ . '/../..' . '/src/benchmarckInterface.php',
+        'douggonsouza\\gentelela\\blocks\\blocks' => __DIR__ . '/../..' . '/src/blocks/blocks.php',
+        'douggonsouza\\gentelela\\controls\\alert' => __DIR__ . '/../..' . '/src/controls/alert.php',
+        'douggonsouza\\gentelela\\identify' => __DIR__ . '/../..' . '/src/identify.php',
+        'douggonsouza\\language\\language' => __DIR__ . '/..' . '/douggonsouza/language/src/language.php',
+        'douggonsouza\\language\\languageInterface' => __DIR__ . '/..' . '/douggonsouza/language/src/languageInterface.php',
+        'douggonsouza\\propertys\\propertys' => __DIR__ . '/..' . '/douggonsouza/propertys/src/propertys.php',
+        'douggonsouza\\propertys\\propertysInterface' => __DIR__ . '/..' . '/douggonsouza/propertys/src/propertysInterface.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
